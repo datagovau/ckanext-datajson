7e43b48a066e06788fa01955efc862e9b0a6a351
gvonness <greg@sitetour.com.au>
2016-04-27 13:45:52 +1000

    - Initial translation of DGA-specific JSON parser to new extension

--- ckanext/datajson/harvester_datajson.py
+++ ckanext/datajson/harvester_datajson.py
@@ -20,7 +20,7 @@ class DataJsonHarvester(DatasetHarvesterBase):
 
     def load_remote_catalog(self, harvest_job):
         try:
-            datasets = json.load(urllib2.urlopen(harvest_job.source.url, None, 90))
+            catalog = json.load(urllib2.urlopen(harvest_job.source.url, None, 90))
         except urllib2.URLError as e:
             log.warn('Failed to fetch %s' % harvest_job.source.url)
             return []
@@ -28,17 +28,10 @@ class DataJsonHarvester(DatasetHarvesterBase):
             log.warn('Failed to parse %s' % harvest_job.source.url)
             return []
 
-        # The first dataset should be for the data.json file itself. Check that
-        # it is, and if so rewrite the dataset's title because Socrata exports
-        # these items all with the same generic name that is confusing when
-        # harvesting a bunch from different sources. It should have an accessURL
-        # but Socrata fills the URL of these in under webService.
-        if datasets != None and len(datasets) > 0 and (datasets[0].get("accessURL") == harvest_job.source.url
-                                                       or datasets[0].get("webService") == harvest_job.source.url) and \
-                        datasets[0].get("title") == "Project Open Data, /data.json file":
-            datasets[0]["title"] = "%s Project Open Data data.json File" % harvest_job.source.title
-
-        return datasets
+        if 'dataset' in catalog:
+            return catalog['dataset']
+        else:
+            return catalog
 
     def set_dataset_info(self, pkg, dataset, harvester_config):
         from pod_to_package import parse_datajson_entry
